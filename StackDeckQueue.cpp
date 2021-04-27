3f0a291f9187ffd20942a790a0ede2436dfe983c
DanilMargaryan <82849114+DanilMargaryan@users.noreply.github.com>
2021-04-27 23:28:30 +0300

    Stack
    создан стек без (init)

--- StackDeckQueue.cpp
+++ StackDeckQueue.cpp
@@ -1,17 +1,103 @@
 ﻿#include <iostream>
 
-class Node
+using namespace std;
+
+class Elem 
 {
-    int key;    // данные
+private:
+    int inf;
+public:
+    Elem* next;
+    
+    Elem(int inf)
+    {
+        this->inf = inf;
+        this->next = nullptr;
+    }
+
+    void print()
+    {
+        cout << inf;
+    }
 };
 
 class Stack
 {
-    Node* head; // указатель на голову стека
+private:
+    Elem* head; // Указатель на начало стека
+
+public:
+    Stack()
+    {
+        this->head = nullptr;
+    }
+
+    void init(int inf)
+    {
+
+	}
+
+    Elem* pop()
+    {
+        Elem* tmp = this->head;
+        
+        this->head = this->head->next;
+        tmp->next = nullptr;
+        return (tmp);
+	}
+
+    void push(Elem* el)
+    {
+        Elem* tmp = this->head;
+
+        if (this->head == nullptr)
+        {
+            this->head = el;
+            return;
+        }
+        while (tmp->next != nullptr)
+            tmp = tmp->next;
+        tmp->next = el;
+    }
+
+    void print()
+    {
+        Elem* tmp = this->head;
+
+        while (tmp != nullptr)
+        {
+            cout << "Elem: ";
+            tmp->print();
+            cout << endl;
+            tmp = tmp->next;
+        }
+	}
 };
 
 int main()
 {
-    std::cout << "Hello Адам завтра в вуз в 7 часов вставать!\n";
-    std::cout << "Вот не повезло!\n";
+    Stack st;                   // Создали стек
+
+    Elem* el = new Elem(5);     
+    st.push(el);
+
+    Elem* el1 = new Elem(4);     
+    st.push(el1);
+
+    Elem* el2 = new Elem(3);     
+    st.push(el2);
+
+    Elem* el3 = new Elem(2);     
+    st.push(el3);
+
+    cout << "Old:" << endl;
+    st.print();
+
+    Elem* el5 = st.pop();
+    (*el5).print();
+
+    cout << "New:" << endl;
+    st.print();
+
+    // Что такое init
 }
