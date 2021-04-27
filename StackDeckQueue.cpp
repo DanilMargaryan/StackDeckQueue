f9d669ea8c3858ba798966c42476088cc7b2af3f
Adam <59186293+AdamS1604@users.noreply.github.com>
2021-04-27 21:33:38 +0300

    Class Node and Stack added

--- StackDeckQueue.cpp
+++ StackDeckQueue.cpp
@@ -1,7 +1,17 @@
 ﻿#include <iostream>
 
+class Node
+{
+    int key;    // данные
+};
+
+class Stack
+{
+    Node* head; // указатель на голову стека
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello Данилу завтра в вуз в 7 часов вставать!\n";
+    std::cout << "Вот не повезло!\n";
 }
-
